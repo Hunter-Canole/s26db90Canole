5cbc9b993a485433c0f615e5990cbf45401df53c
Hunter <hcroyalsfan@gmail.com>
2026-04-22 00:07:05 -0500

    step 7

--- models/jewelry.js
+++ models/jewelry.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose")
 const jewelrySchema = mongoose.Schema({
 name: String,
-cost: Number,
+cost: {
+    type: Number,
+    min: [1, "Cost must be at least $1"],
+    max: [10000, "Cost cannot exceed $10,000"]
+  },
 description: String
 })
 module.exports = mongoose.model("Jewelry",
--- routes/resource.js
+++ routes/resource.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 // Require controller modules.
 var api_controller = require('../controllers/api');
 var jewelry_controller = require('../controllers/jewelries');
 /// API ROUTE ///
 // GET resources base.
 router.get('/', api_controller.api);
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  res.redirect("/login");
+};
+
+// AUTH
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  res.redirect('/');
+});
 /// COSTUME ROUTES ///
 // POST request for creating a Costume.
 router.post('/jewelries', jewelry_controller.jewelry_create_post);
@@ -15,7 +27,7 @@ router.delete('/jewelries/:id', jewelry_controller.jewelry_delete);
 router.put('/jewelries/:id', jewelry_controller.jewelry_update_put);
 // GET request for one Costume.
 router.get('/jewelries/delete', jewelry_controller.jewelry_delete_Page)
-router.get('/jewelries/update', jewelry_controller.jewelry_update_Page);
+router.get('/jewelries/update',secured, jewelry_controller.jewelry_update_Page);
 router.get('/jewelries/create', jewelry_controller.jewelry_create_Page);
 router.get('/jewelries/detail', jewelry_controller.jewelry_view_one_Page);
 router.get('/jewelries/:id', jewelry_controller.jewelry_view_all_Page);
